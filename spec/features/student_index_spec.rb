79f56d2280b1d9c476df2dcb614115f21c457999
Alexandro Disla <alexandrodisla@hotmail.com>
2019-08-23 11:24:48 -0400

    Done.

--- spec/features/student_index_spec.rb
+++ spec/features/student_index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Route to view' do
   it 'has an index page' do
-    visit "/students"
+    visit "/student"
 
     expect(page.status_code).to eq(200)
   end
@@ -13,7 +13,7 @@ describe 'Multiple students are shown' do
     Student.create!(first_name: "Daenerys", last_name: "Targaryen")
     Student.create!(first_name: "Lindsey", last_name: "Stirling")
 
-    visit "/students"
+    visit "/student"
 
     assert_text("Daenerys", "Lindsey")
   end
